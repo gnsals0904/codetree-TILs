9cff27d0b6956b15f4e3408eb553efc369c1c5b0
Hunmin Kim <48405500+gnsals0904@users.noreply.github.com>
2024-07-14 18:12:27 +0900

    Title: 1부터 특정 수까지의 합 2, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240714/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N;
+    static int result;
+    public static void main(String[] args) throws Exception {
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(ir);
+        N = Integer.parseInt(br.readLine());
+        dfs(0);
+        System.out.println(result);
+    }
+
+    static void dfs(int depth){
+        if(depth == N + 1) return;
+        result += depth;
+        dfs(depth + 1);
+    }
+}
