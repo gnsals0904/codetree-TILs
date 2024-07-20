847212869c46ec0daf56c37900cc0b11cf3e25b6
Hunmin Kim <48405500+gnsals0904@users.noreply.github.com>
2024-07-20 21:44:44 +0900

    Title: 원소의 합이 0, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240720/원소의 합이 0/the-sum-of-the-elements-is-0.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N;
+    static int[] A, B, C, D;
+    static HashMap<Integer, Integer> map = new HashMap<>();
+    public static void main(String[] args) throws Exception{
+                InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(ir);
+        N = Integer.parseInt(br.readLine());
+        A = new int[N];
+        B = new int[N];
+        C = new int[N];
+        D = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            C[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            D[i] = Integer.parseInt(st.nextToken());
+        }
+        // input end
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                int temp = A[i] + B[j];
+                if(!map.containsKey(temp)) map.put(temp, 1);
+                else map.put(temp, map.get(temp)+1);
+            }
+        }
+
+        int result = 0;
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                int temp = (C[i] + D[j]) * -1;
+                if(map.containsKey(temp)){
+                    int cnt = map.get(temp);
+                    if(cnt > 0) {
+                        result++;
+                        map.put(temp, cnt - 1);
+                    }
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
