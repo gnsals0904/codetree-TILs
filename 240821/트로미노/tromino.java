7a27fc959a8f52496ce598787418893b79eb9967
Hunmin Kim <48405500+gnsals0904@users.noreply.github.com>
2024-08-21 21:23:38 +0900

    Title: 트로미노, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240821/트로미노/tromino.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, M;
+    static int[][] graph;
+    static int[][] vector = {{1, 0}, {0, 1}};
+    static int result = 0;
+    public static void main(String[] args) throws Exception{
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(ir);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        graph = new int[N][M];
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++){
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+                dfs(i, j, graph[i][j], 1);
+            }
+        }
+        System.out.println(result);
+    }
+
+    static void dfs(int x, int y, int tempSum, int k){
+        if(k > 3){
+            result = Math.max(result, tempSum);
+            return;
+        }
+        for(int[] v: vector){
+            int nx = v[0] + x;
+            int ny = v[1] + y;
+            if(nx >= N || ny >= M) continue;
+            dfs(nx, ny, tempSum + graph[nx][ny], k + 1);
+        }
+    }
+}
