dd16f2ffbe11f694d428dcd1e54c262805446483
Hunmin Kim <48405500+gnsals0904@users.noreply.github.com>
2024-09-20 22:59:36 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240920/피보나치 수/fibonacci-number.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] dp = new int[N + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i = 3; i < N + 1; i++){
+            dp[i] = dp[i - 1] + dp [i - 2];
+        }
+        System.out.println(dp[N]);
+    }
+}
